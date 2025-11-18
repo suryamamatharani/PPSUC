ebd06fc5c3fda5740690c32b21a8ad2faeba9e4d
Mamatharani@123 <suryamamatharani@gmail.com>
2025-11-18 11:24:04 +0530

    Add files via upload

---
+++ array4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a[10],i,n,pos,nv;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	scanf("%d",&pos);
+	scanf("%d",&nv);
+	a[pos]=ny;
+	for(i=0;i<n;i++)
+}
